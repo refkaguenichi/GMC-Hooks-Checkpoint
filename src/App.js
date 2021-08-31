32f6b39105152c1087ea55700dcca56522ed98fc
refkaguenichi <refkaguenichi@gmail.com>
2021-08-31 18:02:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FooterC from "./Components/Footer/Footer";
+import { moviesData } from "./Constantes/MoviesData";
+import MovieList from "./Components/Movies/MovieList";
+import NavbarC from "./Components/Navbar/Navbar";
+import { useState } from "react";
+import Add from "./Components/Add/Add";
 
 function App() {
+  //declare a state to add a new movie by the user
+  const [movie, setMovie] = useState(moviesData);
+  //Add new movie function
+  const addMovie = (newMovie) => {
+    setMovie([newMovie, ...movie]);
+  };
+  //declare a state to search a movie by the user
+  const [text, setText] = useState("");
+  const [newRating, setRating] = useState(0);
+  //search by title function
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setText(e.target.value);
+  };
+  //search by rating function
+  const handleRating = (e, newRating) => {
+    setRating(newRating);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarC
+        handleSearch={handleSearch}
+        handleRating={handleRating}
+        text={text}
+        newRating={newRating}
+      />
+      <Add addMovie={addMovie} />
+      <MovieList items={movie} text={text} newRating={newRating} />
+      <FooterC />
     </div>
   );
 }
